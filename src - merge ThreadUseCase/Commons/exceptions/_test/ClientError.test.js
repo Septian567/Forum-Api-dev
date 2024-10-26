cbbc4f13fdbdffe87b4dd8c950f1bce242c26819
Septian Dika Maulana <99572215+Septian567@users.noreply.github.com>
2024-10-26 20:56:45 +0700

    Delete src - merge ThreadUseCase directory

--- src - merge ThreadUseCase/Commons/exceptions/_test/ClientError.test.js
+++
@@ -1,9 +0,0 @@
-const ClientError = require("../ClientError");
-
-describe("ClientError", () => {
-  it("must raise an error when instantiated directly", () => {
-    expect(() => new ClientError("")).toThrowError(
-      "cannot instantiate abstract class"
-    );
-  });
-});
