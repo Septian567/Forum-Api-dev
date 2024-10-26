7e38ff5a396142773c29574a4a82b54da21197db
Septian Dika Maulana <99572215+Septian567@users.noreply.github.com>
2024-10-26 20:57:23 +0700

    Delete src -1 directory

--- src -1/Applications/security/OwnerValidator.js
+++
@@ -1,7 +0,0 @@
-class OwnerValidator {
-   async verifyOwner(credentialId, ownerId, entityType) {
-      throw new Error('OWNER_VALIDATOR.METHOD_NOT_IMPLEMENTED');
-   }
-}
-
-module.exports = OwnerValidator;
